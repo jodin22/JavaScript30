720a5243283ea98e1485a80b97e104b2e4d5fa27
jodin22sams <jeromekrwc@gmail.com>
2022-09-11 04:05:57 -0700

    edit comments.

--- 01 - JavaScript Drum Kit/drumkit.js
+++ 01 - JavaScript Drum Kit/drumkit.js
@@ -43,16 +43,16 @@ window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); // get the audio tag with data-key attribute and put
     // in a var
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); // get the class key with data-key attribute and put in a var
-    // notice this line is almost identical to the one above it. bc the data-key match you want is the key that is pressed.
+    // notice this line is almost identical to the one above it bc you want the same data-key for the audio and key pressed.
     // since the prev line was finding the audio tag data-key attribute, this selector is finding the class='key' with the same
-    // data-key attribute. when the keyCode equals some number, you can now have the audio data-key and the class='key'
+    // data-key attribute. the keyCode of the key that was pressed is what links up with the correct audio to play
     if (!audio) { // !audio is the opposite of audio meaning it doesn't exist on your page
     return; // this will exit the function(e) immediately
     }
     else {  // for those that exist, it prints to the console and plays a sound
     console.log(audio); // <audio data-key='65' src=sound file>
-    audio.currentTime = 0;
-    audio.play();
+    audio.currentTime = 0; // reset the sound
+    audio.play(); // play the sound
     console.log(key); // <div data-key="65" class="key">  the corresponding attributes that we need to connect our animation to 
     //  the html
     key.classList.add('playing'); // at this line, it adds the class='playing' and now the div has class='key playing' which will 
@@ -64,9 +64,10 @@ from the css transition time if anyone changes the .07s to something else. inste
 of a setTimeOut function.
 
 an event is when a click or keydown happens. then js recognizes that event and knows the event happened which can 
-then do some code. a transition end event is where js will see that nothing was clicked but saw that a transition (or change)
-happened and can still do some code. in this ex, the transition is that the scale is changing in the css and the border color 
-also changed or transitioned. */
+then do some code. a transition end event is useful bc even when no click or key press was done, but a tag or css property
+changed, js will see that a change or transition occurred and can do some code after this transition ends. in this ex, the 
+transition is that the scale is changing in the css and the border color also changed or transitioned.
+*/
 
     function removeTransition(e) {  // in the console, it will show 6 transitionend events bc the element that changed has 
         // 4 border colors, 1 box shadow and 1 transform which totals 6. to limit just the transform, edit the e.
@@ -76,12 +77,12 @@ also changed or transitioned. */
         console.log(this); // 'this' shows what was called against it. in this ex, 'this' is the class=key from the arrow function 
         // below. the key is the element from the node list that the event listener is on when it is passed to the 
         // removeTransition function. <div data-key='65'  class=key>
-        this.classList.remove('playing');
+        this.classList.remove('playing');  // <div data-key='65' class = key playing> becomes <div data-key='65' class = key>
         console.log(keys); // shows all the nodes for class=key and no longer shows class=key playing bc of the remove line above
     };
 
     const keys = document.querySelectorAll('.key'); // selects all the class=key. puts it into an array-like aka node list
-    /* window.addEventListener('transitionend', function(e) {  this is same as line 73. it will show all 6 properties which 
+    /* window.addEventListener('transitionend', function(e) {  this is same as line 74. it will show all 6 properties which 
         we don't want. we only want to see the transform property
         console.log(e);
     }) */
@@ -92,7 +93,7 @@ also changed or transitioned. */
 
 });
 
-/* now that we can get the data-key and class attributes, we can connect our style sheet code to the elements which will show 
+/* the data-key and class attributes are used to connect the sound and style sheet code to the elements which will show 
 the animation each time a key is pressed along with the audio
 */
 
